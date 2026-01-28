583b14e413bbbe38b8842a133ea1701eb9839f49
LakshyaDoshi <lakshyadoshi97@gmail.com>
2026-01-28 22:31:23 +0530

    Dirty2

--- Display.c
+++ Display.c
@@ -25,12 +25,12 @@ void display(char board[3][3]){
 
 int main(){ 
     char board[3][3]={'1','2','3','4','5','6','7','8','9'};
-
+    int valid =0;
     display(board);
     char Player1,Player2;
 
     do{
-        //Player 1
+        while(valid!=1){
         printf("Place your EX (Player 1): ");
         scanf(" %c",&Player1);
         for(int i=0;i<3;i++){
@@ -38,13 +38,16 @@ int main(){
                 if(board[i][j]!='X' && board[i][j]!='O'){
                     if(board[i][j]==Player1){
                         board[i][j]='X';
+                        valid=1;
                     }
+                  }
                 }
             }   
-        }   
+           
         display(board);
-
+        }
         //Player 2
+        while(valid!=1){
         printf("Place your O (Player 2): ");
         scanf(" %c",&Player2);
         for(int i=0;i<3;i++){
@@ -52,10 +55,12 @@ int main(){
                 if(board[i][j]!='X' && board[i][j]!='O'){
                     if(board[i][j]==Player2){
                         board[i][j]='O';
+                        valid=1;
                     }
                 }
             }   
         }   
         display(board);
+    }
     }while(1);
 }
