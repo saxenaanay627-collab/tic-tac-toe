3bdf45de0f02151bdbb451eb7dbb7a40dcd39b09
saxenaanay627 <saxenaanay627@gmail.com>
2026-01-28 23:03:58 +0530

    yo

--- logic.c
+++ logic.c
@@ -3,16 +3,24 @@
 
 int checkwin(char board[3][3]) {
     for(int i=0;i<3;i++){
-        if(board[i][0]==board[i][1] && board[i][1]==board[i][2])
+        if(board[i][0]==board[i][1] && board[i][1]==board[i][2]){
+            printf("%c wins",board[i][0]);
+        }
             return 1;
-        if(board[0][i]==board[1][i] && board[1][i]==board[2][i])
+        if(board[0][i]==board[1][i] && board[1][i]==board[2][i]){
+            printf("%c wins",board[0][i]);
+        }
             return 1;
     }
 
-    if(board[0][0]==board[1][1] && board[1][1]==board[2][2])
+    if(board[0][0]==board[1][1] && board[1][1]==board[2][2]){
+        printf("%c wins",board[0][0]);
+    }
         return 1;
 
-    if(board[0][2]==board[1][1] && board[1][1]==board[2][0])
+    if(board[0][2]==board[1][1] && board[1][1]==board[2][0]){
+        printf("%c wins",board[1][1]);
+    }
         return 1;
 
     return 0;
